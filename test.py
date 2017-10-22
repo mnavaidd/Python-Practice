d21d4829908a15c504baf816eb21941f4926cbb6
mnavaidd <33001052+mnavaidd@users.noreply.github.com>
2017-10-22 23:08:07 +0500

    Hello
    hello

---
+++ test.py
@@ -0,0 +1,12 @@
+ListOfNUmbers = [1,2,3,4,5,6,7,8,9,10]
+
+for number in ListOfNUmbers:
+    print (number);
+    if (number % 2 == 0):
+        print ("is odd number");
+    else:
+        print ("is even number");
+
+
+
+
